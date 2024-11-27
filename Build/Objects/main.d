bb94d6b214cf3cd87d0a5ee8b6bbb998606a16d7
Yuchen <jiangyuchenjiang@gmail.com>
2024-11-27 23:30:48 +0800

    添加参数后编译下载成功

--- Build/Objects/main.d
+++ Build/Objects/main.d
@@ -3,4 +3,5 @@
   C:\Users\70971\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
   ..\Src\BSP\MCU\system_TM4C123.h ..\Src\BSP\Hal.h ..\Src\BSP\BSP.h \
   ..\Src\App\LED\LED.h ..\Src\BSP\IRQ.h ..\Src\BSP\SPI\spim.h \
-  ..\Src\App\LCD\LCD_ST7735R.h ..\Src\App\LCD\gui.h ..\Src\BSP\UART.h
+  ..\Src\App\LCD\LCD_ST7735R.h ..\Src\App\LCD\gui.h ..\Src\BSP\UART.h \
+  ..\Src\App\HT1621\HT1621.h
